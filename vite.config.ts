10098c245d2cc915a2c3da6838dd6484213062a9
JakeAve <jacobjavery@gmail.com>
2026-05-20 20:52:14 -0600

    feat: configure vite-plugin-pwa in injectManifest mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [fresh(), tailwindcss()],
+  plugins: [
+    fresh(),
+    tailwindcss(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: ".",
+      filename: "sw.ts",
+      injectRegister: false,
+      manifest: false,
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
   server: { strictPort: true },
 });
